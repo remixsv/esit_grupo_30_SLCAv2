5ca7895b26a2501bdddc45ed99e2e3205c57c8a1
remixsv <bkrdjsv+1@gmail.com>
2025-08-27 22:33:38 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
-  var iframe = document.getElementById('appointmentIframe');
-  if (iframe.style.display === 'none') {
-    iframe.style.display = 'block';
-  } else {
-    iframe.style.display = 'none';
-  }
-});
